10c234c92771fd8335671bbf5a38825d3cae0917
tech4GT <varun.gupta1798@gmail.com>
2018-05-25 01:05:40 +0530

    blend complete
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/modules/Blend/Module.js
+++ src/modules/Blend/Module.js
@@ -24,47 +24,45 @@ module.exports = function Dynamic(options, UI, util) {
 
     // save first image's pixels
     var priorStep = getStep(-2);
-    console.log(priorStep.options.name);
 
     getPixels(priorStep.output.src, function (err, pixels) {
       options.firstImagePixels = pixels;
+
+      function changePixel(r2, g2, b2, a2, x, y) {
+        // blend!
+        var p = options.firstImagePixels;
+        return options.func(
+          r2, g2, b2, a2,
+          p.get(x, y, 0),
+          p.get(x, y, 1),
+          p.get(x, y, 2),
+          p.get(x, y, 3)
+        )
+      }
+
+      function output(image, datauri, mimetype) {
+
+        // This output is accessible by Image Sequencer
+        step.output = { src: datauri, format: mimetype };
+
+        // This output is accessible by the UI
+        options.step.output = datauri;
+
+        // Tell the UI that the draw is complete
+        UI.onComplete(options.step);
+
+      }
+
+      // run PixelManipulatin on second image's pixels 
+      return require('../_nomodule/PixelManipulation.js')(input, {
+        output: output,
+        changePixel: changePixel,
+        format: input.format,
+        image: options.image,
+        inBrowser: options.inBrowser,
+        callback: callback
+      });
     });
-
-    function changePixel(r2, g2, b2, a2, x, y) {
-      // blend!
-      var p = options.firstImagePixels;
-      return options.func(
-        r2, g2, b2, a2,
-        p.get(x, y, 0),
-        p.get(x, y, 1),
-        p.get(x, y, 2),
-        p.get(x, y, 3)
-      )
-    }
-
-    function output(image, datauri, mimetype) {
-
-      // This output is accessible by Image Sequencer
-      step.output = { src: datauri, format: mimetype };
-
-      // This output is accessible by the UI
-      options.step.output = datauri;
-
-      // Tell the UI that the draw is complete
-      UI.onComplete(options.step);
-
-    }
-
-    // run PixelManipulatin on second image's pixels 
-    return require('../_nomodule/PixelManipulation.js')(input, {
-      output: output,
-      changePixel: changePixel,
-      format: input.format,
-      image: options.image,
-      inBrowser: options.inBrowser,
-      callback: callback
-    });
-
   }
 
   return {
