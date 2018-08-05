743aaa0c30346e5fea5aae9b1923f3c3b9ce3215
tech4GT <varun.gupta1798@gmail.com>
2018-08-05 14:04:16 +0530

    add inputs to colorbar
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- dist/image-sequencer.js
+++ dist/image-sequencer.js
@@ -48742,13 +48742,17 @@ module.exports={
 }
 
 },{}],162:[function(require,module,exports){
-module.exports = function NdviColormapfunction() {
+module.exports = function NdviColormapfunction(options, UI) {
 
+    options.x = options.x || 0;
+    options.y = options.y || 0;
+    options.colormap = options.colormap || "default";
+    options.h = options.h || 10;
     this.expandSteps([
         { 'name': 'gradient', 'options': {} },
-        { 'name': 'colormap', 'options': {} },
-        { 'name': 'crop', 'options': { 'y': 0, 'h': 10 } },
-        { 'name': 'overlay', 'options': { 'x': 0, 'y': 0, 'offset': -4 } }
+        { 'name': 'colormap', 'options': { colormap: options.colormap } },
+        { 'name': 'crop', 'options': { 'y': 0, 'h': options.h } },
+        { 'name': 'overlay', 'options': { 'x': options.x, 'y': options.y, 'offset': -4 } }
     ]);
     return {
         isMeta: true
@@ -48760,7 +48764,34 @@ arguments[4][147][0].apply(exports,arguments)
 module.exports={
     "name": "Colorbar",
     "description": "Generates a colorbar to lay over the image",
-    "inputs": {},
+    "inputs": {
+        "colormap": {
+            "type": "select",
+            "desc": "Name of the Colormap",
+            "default": "default",
+            "values": [
+                "default",
+                "greyscale",
+                "stretched",
+                "fastie"
+            ]
+        },
+        "x": {
+            "type": "integer",
+            "desc": "X-position of the image on which the new image is overlayed",
+            "default": 0
+        },
+        "y": {
+            "type": "integer",
+            "desc": "Y-position of the image on which the new image is overlayed",
+            "default": 0
+        },
+        "h": {
+            "type": "iinteger",
+            "desc": "height of the colorbar",
+            "default": 10
+        }
+    },
     "length": 4
 }
 },{}],165:[function(require,module,exports){
