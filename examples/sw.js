7786dde5529a5dd51fc0cc56e7a7594c80c188cb
Mridul Aggarwal <mridulaggarwal97@gmail.com>
2018-09-11 22:46:01 +0530

    cache the files on network request

--- examples/sw.js
+++ examples/sw.js
@@ -1,33 +1,7 @@
-const filesToCache = [
-  '/examples/#steps=',
-  '/node_modules/jquery/dist/jquery.min.js',
-  '/node_modules/bootstrap/dist/js/bootstrap.min.js',
-  '/src/ui/prepareDynamic.js',
-  '/dist/image-sequencer.js',
-  '/examples/lib/urlHash.js',
-  '/examples/lib/defaultHtmlStepUi.js',
-  '/examples/lib/defaultHtmlSequencerUi.js',
-  '/examples/demo.js',
-  '/node_modules/imgareaselect/jquery.imgareaselect.dev.js',
-  '/node_modules/bootstrap/dist/css/bootstrap.min.css',
-  '/node_modules/font-awesome/css/font-awesome.min.css',
-  '/node_modules/imgareaselect/distfiles/css/imgareaselect-default.css',
-  '/examples/demo.css',
-  'https://raw.githubusercontent.com/theleagueof/league-spartan/master/_webfonts/leaguespartan-bold.woff2',
-  '/node_modules/font-awesome/fonts/fontawesome-webfont.woff2?v=4.5.0',
-  '/examples/images/tulips.png'
-];
-
 const staticCacheName = 'image-sequencer-static-v1';
 
 self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
-  event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
+  console.log('Attempting to install service worker');
 });
 
 self.addEventListener('activate', function(e) {
@@ -47,10 +21,16 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          if(event.request.method == "GET")
+            cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
+
