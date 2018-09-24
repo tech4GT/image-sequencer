7667928ab4af18b01855248b219e38a4198c38fc
tech4GT <varun.gupta1798@gmail.com>
2018-09-24 11:10:47 +0530

    caching on first run
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- examples/demo.js
+++ examples/demo.js
@@ -28,10 +28,10 @@ window.onload = function() {
   $("#addStep select").on("change", ui.selectNewStepUi);
   $("#addStep #add-step-btn").on("click", ui.addStepUi);
   $('#addStep #download-btn').click(function() {
-    $('img:last()').trigger( "click" );
- 
+    $('img:last()').trigger("click");
+
     return false;
-    });
+  });
   $('body').on('click', 'button.remove', ui.removeStepUi);
   $('#save-seq').click(() => {
     sequencer.saveSequence(window.prompt("Please give a name to your sequence..."), sequencer.toString());
@@ -53,12 +53,19 @@ window.onload = function() {
   });
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js',{scope : '/examples/'})
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
+    navigator.serviceWorker.register('sw.js', { scope: '/examples/' })
+      .then(function(registration) {
+        const installingWorker = registration.installing;
+        installingWorker.onstatechange = () => {
+          console.log(installingWorker)
+          if (installingWorker.state === 'installed') {
+            location.reload();
+          }
+        }
+        console.log('Registration successful, scope is:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service worker registration failed, error:', error);
+      });
   }
 };
