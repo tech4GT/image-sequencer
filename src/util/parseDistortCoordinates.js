023b2dbf052890f8b8d6de773a2c73f23f98985f
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-04-19 22:13:17 +0530

    test fix and input parse

--- src/util/parseDistortCoordinates.js
+++ src/util/parseDistortCoordinates.js
@@ -1,10 +1,10 @@
 module.exports = function parseDistortCoordinates(options) {
 
   let coord = []
-  coord.push(options.nw.split(' '))
-  coord.push(options.ne.split(' '))
-  coord.push(options.se.split(' '))
-  coord.push(options.sw.split(' '))
+  coord.push(options.nw.split(','))
+  coord.push(options.ne.split(','))
+  coord.push(options.se.split(','))
+  coord.push(options.sw.split(','))
 
   let parsedCoord = coord.reduce((acc, val) => {
     acc.push(parseInt(val[0]))
--- test/core/modules/webgl-distort.js
+++ test/core/modules/webgl-distort.js
@@ -6,14 +6,14 @@ require('../../../src/ImageSequencer.js');
 
 var sequencer = ImageSequencer({ ui: false });
 var options = {
-        nw: '0 100',
-        ne: '1023 50',
-        se: '1223 867',
-        sw: '100 767'
+        nw: '0 0',
+        ne: '10 4',
+        se: '10 5',
+        sw: '0 4'
     };
 var target = 'test_outputs';
 var red = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABlBMVEX+AAD///+KQee0AAAAAWJLR0QB/wIt3gAAAAd0SU1FB+EGHRIVAvrm6EMAAAAMSURBVAjXY2AgDQAAADAAAceqhY4AAAAldEVYdGRhdGU6Y3JlYXRlADIwMTctMDYtMjlUMTg6MjE6MDIrMDI6MDDGD83DAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE3LTA2LTI5VDE4OjIxOjAyKzAyOjAwt1J1fwAAAABJRU5ErkJggg==";
-var benchmark = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAH0lEQVQ4T2NkoBAwUqifYdQAhtEwYBgNA1A+Gvi8AAAmmAARf9qcXAAAAABJRU5ErkJggg==";
+var benchmark = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAp0lEQVQ4T62SsQ3CMBBF3y/YgDUYAQE9IzACEvS0LAAZgRGoYQZGAXpzKFISWYmxzsCV1v/vnnTWCwy4CRaCO4WjBtDVBFvBwcsZAKJibTUXPHKwHMBl5QL0rGaCZ/tWCoitNoLj14CfDf4HqEkGS4Oz9/ZxTv1SgJNg5YUNAG3RYGpwAUbZj+TZFKASrFPZjwapsMHE4AqMuyt4DFKZAHvBrsggBXoDQmQp9u/Xo6IAAAAASUVORK5CYII=";
 
 // Test 1 to check webgl-distort module is getting loaded
 test('Load distort module', function(t) {
@@ -25,10 +25,10 @@ test('Load distort module', function(t) {
 
 // Test 2 to check options are correct
 test('Check Options', function(t) {
-  t.equal(sequencer.steps[1].options.nw, '0 100', 'Options are correct');
-  t.equal(sequencer.steps[1].options.ne, '1023 50', 'Options are correct');
-  t.equal(sequencer.steps[1].options.se, '1223 867', 'Options are correct');
-  t.equal(sequencer.steps[1].options.sw, '100 767', 'Options are correct');
+  t.equal(sequencer.steps[1].options.nw, '0 0', 'Options are correct');
+  t.equal(sequencer.steps[1].options.ne, '10 4', 'Options are correct');
+  t.equal(sequencer.steps[1].options.se, '10 5', 'Options are correct');
+  t.equal(sequencer.steps[1].options.sw, '0 4', 'Options are correct');
   t.end();
 });
 
