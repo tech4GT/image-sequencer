5dc6d7e6e6fcfed8d34da0fe659765145d37758a
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-04-19 22:18:55 +0530

    test fix

--- test/core/modules/webgl-distort.js
+++ test/core/modules/webgl-distort.js
@@ -6,10 +6,10 @@ require('../../../src/ImageSequencer.js');
 
 var sequencer = ImageSequencer({ ui: false });
 var options = {
-        nw: '0 0',
-        ne: '10 4',
-        se: '10 5',
-        sw: '0 4'
+        nw: '0,0',
+        ne: '10,4',
+        se: '10,5',
+        sw: '0,4'
     };
 var target = 'test_outputs';
 var red = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABlBMVEX+AAD///+KQee0AAAAAWJLR0QB/wIt3gAAAAd0SU1FB+EGHRIVAvrm6EMAAAAMSURBVAjXY2AgDQAAADAAAceqhY4AAAAldEVYdGRhdGU6Y3JlYXRlADIwMTctMDYtMjlUMTg6MjE6MDIrMDI6MDDGD83DAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE3LTA2LTI5VDE4OjIxOjAyKzAyOjAwt1J1fwAAAABJRU5ErkJggg==";
@@ -25,10 +25,10 @@ test('Load distort module', function(t) {
 
 // Test 2 to check options are correct
 test('Check Options', function(t) {
-  t.equal(sequencer.steps[1].options.nw, '0 0', 'Options are correct');
-  t.equal(sequencer.steps[1].options.ne, '10 4', 'Options are correct');
-  t.equal(sequencer.steps[1].options.se, '10 5', 'Options are correct');
-  t.equal(sequencer.steps[1].options.sw, '0 4', 'Options are correct');
+  t.equal(sequencer.steps[1].options.nw, '0,0', 'Options are correct');
+  t.equal(sequencer.steps[1].options.ne, '10,4', 'Options are correct');
+  t.equal(sequencer.steps[1].options.se, '10,5', 'Options are correct');
+  t.equal(sequencer.steps[1].options.sw, '0,4', 'Options are correct');
   t.end();
 });
 
