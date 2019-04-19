7de33cc7e50ae9edfe002196d93a431f1ff6f857
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-04-19 09:40:11 +0530

    WIP

---
+++ src/util/parseDistortCoordinates.js
@@ -0,0 +1,11 @@
+module.exports = function(options) {
+
+  let coord = []
+  coord.push(options.nw.split(' '))
+  coord.push(options.ne.split(' '))
+  coord.push(options.sw.split(' '))
+  coord.push(options.se.split(' '))
+
+  let parsedCoord = coord.reduce()
+
+}
